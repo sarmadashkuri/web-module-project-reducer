d8b3b6c32a224ab948ff0922481f1e690b1a602b
Sarmad Ashkuri <sarmadashkuri@gmail.com>
2023-06-30 02:17:18 -0400

    met mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const SET_MEMORY = "SET_MEMORY";
+export const MEMORY_RECALL = "MEMORY_RECALL";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return ({type:CHANGE_OPERATION, payload:operator});
+}
+
+export const clearDisplay = () => {
+    return ({type:CLEAR_DISPLAY})
+}
+
+export const setMemory = () => {
+    return({type:SET_MEMORY});
+}
+
+export const memoryRecall = () => {
+    return({type:MEMORY_RECALL});
+}
+
+export const memoryClear = () => {
+    return({type:MEMORY_CLEAR});
 }
